1b13e17b009c9ba20dee9ff73d2d936dcc2b17c9
Dmitri Kiselevich <dmitri.web.master@gmail.com>
2014-01-23 15:29:39 +0200

    add presence val for customer

--- source/app/models/customer.rb
+++ source/app/models/customer.rb
@@ -1,6 +1,7 @@
 class Customer < ActiveRecord::Base
   has_many :charges
   has_one :charge, ->{ order("amount desc") }
+  validates :first_name, :last_name, :email, presence: true
 
   def full_name
     [first_name, last_name].join(" ")
